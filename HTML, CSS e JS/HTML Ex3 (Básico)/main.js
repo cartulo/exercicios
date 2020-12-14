515484b3a0555eced4cc6d86d55e894b9ecee992
Gilberto Júnior <gilbertocolossus@hotmail.com>
2020-12-14 19:33:11 -0300

    Delete main.js

--- HTML, CSS e JS/HTML Ex3 (Básico)/main.js
+++
@@ -1,8 +0,0 @@
-        function validaForm() {
-            nome = document.getElementById("nome").value;
-            empr = document.getElementById("empresas").value;
-            hora = document.getElementById("horas").value;
-            if (nome == "" || empr == "" || hora == "") {
-                alert("Todos campos devem ser preenchidos!");
-                return false;
-            
