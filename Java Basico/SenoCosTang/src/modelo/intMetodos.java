48c4d11c7bfed6b7ef490529d5c71dc084a21b4d
Gilberto Júnior <gilbertocolossus@hotmail.com>
2021-05-14 00:35:04 -0300

    Adicionado novo projeto Java, este por sua vez, calcula os valores de Seno, Cosseno e Tangente ao informar os graus. Mais informacoes no README.md da pasta Java Basico

---
+++ Java Basico/SenoCosTang/src/modelo/intMetodos.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public interface intMetodos
+    {
+        public void Executar();
+    }
+
+
---
+++ Java Basico/SenoCosTang/src/senocostang/SenoCosTang.java
@@ -0,0 +1,14 @@
+package senocostang;
+
+import apresentacao.frmPrincipal;
+
+public class SenoCosTang
+    {
+
+    public static void main(String[] args)
+        {
+        frmPrincipal frmP = new frmPrincipal(null, true);
+        frmP.setVisible(true);
+        }
+    
+    }
